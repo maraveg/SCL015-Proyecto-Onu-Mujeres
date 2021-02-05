3962cc7ca034bfc4acee6d8798862c3208b88bd0
maraveg <taravenaguerra@gmail.com>
2021-02-05 10:05:37 -0300

    Inicio de enroutamiento

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,1 +1,42 @@
 //import { welcome } from "./view/templateIngresoUsuaria.js";
+import { welcome } from './lib/view/templateBienvenida.js';
+//import { editProfile } from './lib/view/templateEditarPerfil.js';
+import { logIn } from './lib/view/templateIngresoUsuaria.js';
+//import { signUp } from './lib/view/templateRegistroUsuaria.js';
+//import { tutorial } from './lib/view/templateTutorial.js';
+//import { channels } from './lib/view/templateCanales.js';
+//import { profile } from './lib/view/templatePerfil.js';
+//import { community } from './lib/view/templateComunidad.js';
+//import { communitySearch } from './lib/view/templateComunidadBuscador.js';
+//import { passwordReset } from './lib/view/templateRecuperarContrasena.js';
+//import { inbox } from './lib/view/templateMensajes.js';
+//import { message } from './lib/view/templateEnviarMensaje.js';
+//import { language } from './lib/view/templateIdioma.js';
+import { menu } from './lib/view/templateMenu.js';
+
+const showTemplate = (hash) => {
+  const containerRoot = document.getElementById('root');
+  containerRoot.innerHtml = menu();
+
+  switch (hash) {
+    case '#/':
+      containerRoot.appendChild(welcome());
+      break;
+    case '#/logIn':
+      containerRoot.appendChild(logIn());
+      break;
+    default:
+      containerRoot.innerHTML = '<h2>No existe :c</h2>';
+  }
+};
+
+export const changeRoute = (hash) => {
+  if (hash === '#/') {
+    return showTemplate(hash);
+  } else if (hash === '#/logIn') {
+    return showTemplate(hash);
+    
+  } else {
+    return showTemplate(hash);
+  }
+};
