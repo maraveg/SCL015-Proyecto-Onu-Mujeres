38253e6234707c00a50e301ba425fb1ae0603434
Llaira <llairareyesd@gmail.com>
2021-02-10 13:46:38 -0600

    página resgitro de usuria modificada

--- src/lib/view/templateRegistroUsuaria.js
+++ src/lib/view/templateRegistroUsuaria.js
@@ -8,24 +8,26 @@ export const signUp = () => {
         <img  id="onu-logo"  src="./assets/onu.women.png">
         </div>
         <p id="Registry">Registro</p>
-        <div class="name-surname">
-        <input type="text" placeholder="Nombre" id="text-name">
-        <input type="text" placeholder="Apellido" id="text-surname">
+        <div class="input-placeholder">
+        <input type="text" placeholder="Nombre" id="text-name" class="name-surname">
+        <input type="text" placeholder="Apellido" id="text-surname" class="name-surname">
         </div>
+        <select name="Regions" id="region">Regiones
+        <option value="0" selected disabled>Zona</option>
+        <option value="Ant">Zona Norte</option></div>
+        <option value="Met">Zona Centro</option></div>
+        <option value="Ara">Zona Sur</option></div>
+        </select></div>
+        <select id="city">
+        <option value="0" selected disabled id="color-city">Ciudad</option>
+        <option value="Ant">Antofagasta</option></div>
+        <option value="Met">Santiago</option></div>
+        <option value="Ara">Temuco/option></div>
+        </select></div>
         <div class="input-placeholder"><input type="email" placeholder="E-mail" id="text-mail"></div>
         <div class="input-placeholder"><input type="password" placeholder="Contraseña" id="text-password"></div>
         <div class="input-placeholder"><input type="password" placeholder="Repetir Contraseña" id="text-repeat-password"></div>
-        <div id="select-regions"><select name="Regions" id="region">Regiones</div>
-        <div id="><option value="0" selected disabled>Regiones</option>
-        <optgroup label="Zona Norte"></optgroup></div>
-        <option value="Ant">Antofagasta</option></div>
-        <optgroup label="Zona Centro"></optgroup></div>
-        <option value="Met">Metropolitana</option></div>
-        <optgroup label="Zona Sur"></optgroup></div>
-        <option value="Ara">Araucanía</option></div>
-        </select></div>
-        <div class="input-placeholder"><input type="text" placeholder="Ciudad"id="text-city"></div>
-        <p>Al clickear "Registrarme" aceptas nuestras Condiciones, la Politica de datos y la Politica de cookies.</p>
+        <p id="conditions">Al clickear "Registrarme" aceptas nuestras Condiciones, la Politica de datos y la Politica de cookies.</p>
         <button id="signup-button">Registrarme</button>
     `;
     divSignUp.innerHTML = viewSignUp;
