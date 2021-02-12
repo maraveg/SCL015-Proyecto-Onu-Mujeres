81295e60cd0eed1571d8e09144279f234014775f
Llaira <llairareyesd@gmail.com>
2021-02-12 13:37:14 -0600

    Detalles en css template comunidad

--- src/lib/view/templateComunidad.js
+++ src/lib/view/templateComunidad.js
@@ -9,27 +9,27 @@ export const community = () => {
         </div>
         <p id="text-zone">¡Selecciona una zona y comunícate
         con más mujeres como tú!</p>
-        <p><a href="#/north">Zona Norte</a></p>
+        <p><a href="#/north" class="Zones">Zona Norte</a></p>
         <div>
             <div>
             <img class="user" src="./assets/user.png">
             </div>
         </div>
-        <p><a href="#/north"><strong>Ver a todas</strong></a></p>
-        <p><a href="#/center">Zona Centro</a></p>
+        <p><a href="#/north" class="see-all"><strong>Ver a todas</strong></a></p>
+        <p><a href="#/center" class="Zones">Zona Centro</a></p>
         <div>
             <div>
             <img class="user" src="./assets/user.png">
             </div>
         </div>
-        <p><a href="#/center"><strong>Ver a todas</strong></a></p>
-        <p><a href="#/south">Zona Sur</a></p>
+        <p><a href="#/center" class="see-all"><strong>Ver a todas</strong></a></p>
+        <p><a href="#/south" class="Zones">Zona Sur</a></p>
         <div>
             <div>
             <img class="user" src="./assets/user.png">
             </div>
         </div>
-        <p><a href="#/south"><strong>Ver a todas</strong></a></p>
+        <p><a href="#/south" class="see-all"><strong>Ver a todas</strong></a></p>
     `;
     divCommunity.innerHTML = viewCommunity;
     return divCommunity;
