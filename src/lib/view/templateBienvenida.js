2447f7aa4dc8df878d915bd56e6fff89c4dbceb5
Llaira <llairareyesd@gmail.com>
2021-02-12 22:39:17 -0600

    Modifiqué el templateBienvenida para agregar idiomas - cambié fondo del color entrega por las ux en el css

--- src/lib/view/templateBienvenida.js
+++ src/lib/view/templateBienvenida.js
@@ -5,9 +5,10 @@ export const welcome = () => {
         <img src="./assets/lightBlue.jpg" class="fondo-welcome">
         <p id="text-welcome" >Bienvenida</p>
         <img src="./assets/logo.png" class="logo-bienvenida">
+        <p class="english"><a href="#/" id="english" >English</a></p>
+        <p class="spanish"><a href="#/" id="spanish" >Español</a></p>
         <button id="enter-button">Entrar</button> </div>
-        <p><a href="#/" id="english">English</a></p>
-        <p><a href="#/" id="spanish">Español</a></p>
+       
     `;
     divWelcome.innerHTML = viewWelcome;
     const loginForm = divWelcome.querySelector('#enter-button');
--- src/lib/view/templateBuscadorComunidad.js
+++ src/lib/view/templateBuscadorComunidad.js
@@ -3,7 +3,10 @@ export const communitySearch = () => {
     const viewCommunitySearch = `
         <div class:"logos"> 
         <img  id="oportunidad-logo" src="./assets/logo.png"></div>
-        <p>COMUNIDAD</p>
+        <div class="community-arrow">
+        <img src="./assets/left-arrow.png" id="icon-arrow">
+        <p id="text-community">Comunidad</p>
+        </div>
         <input type="search" id="SearchUser">
     `;
     divCommunitySearch.innerHTML = viewCommunitySearch;
