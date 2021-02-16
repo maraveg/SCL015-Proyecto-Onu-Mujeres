79f302367ed59a8e297e81a5efa48f80a21c16ff
cecyramallo <cecyramallo@gmail.com>
2021-02-16 08:51:59 -0400

    Primer intento de register con Firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,49 +1,7 @@
-export const createAccount = (firstName, lastName, email, password) => {
-    firebase.auth().createUserWithEmailAndPassword(email, password);
-        .then(() => {
-            const signUp = firebase.auth().currentUser;
-            signUp.updateProfile({
-                displayName: firstName, lastName
-            })
-            signUp.sendVerificationEmail();
-            alert("Revisa el email de verificación que te enviamos");
-            window.location.href = "";
-        })
-}
+console.log("Hello!");
 
-//     .then(result => {
-//         result.user.updateProfile({
-//             displayName : names
-//         })
-
-//         const configuration = {
-//             url : 'http://localhost:5000/'
-//         }
-
-//         result.user.sendVerificationEmail(configuration)
-//         .catch(error => {
-//             console.error(error)
-//             Materialize.toast(error.message, 4000)            
-//     })
-
-//     firebase.auth().signOut()
-
-//     Materialize.toast("Welcome ${names}, you must verify your account")
-
-//     $('.modal').modal('close')
-
-//     .catch(error => {
-//         console.error(error)
-//         Materialize.toast(error.message, 4000)
-//     })
-
-// }
-
-
-// console.log("Hello!");
-
-// export const myFunction = () => {
-//     //aqui tu codigo
-//     console.log('Hola mundo!');
+export const myFunction = () => {
+    //aqui tu codigo
+    console.log('Hola mundo!');
     
-// };
+};
--- src/lib/view/templateRegistroUsuaria.js
+++ src/lib/view/templateRegistroUsuaria.js
@@ -35,3 +35,44 @@ export const signUp = () => {
     })
     return divSignUp;
 };
+
+// export const createAccount = (firstName, lastName, email, password) => {
+//   firebase.auth().createUserWithEmailAndPassword(email, password);
+//       .then(() => {
+//           const signUp = firebase.auth().currentUser;
+//           signUp.updateProfile({
+//               displayName: firstName, lastName
+//           })
+//           signUp.sendVerificationEmail();
+//           alert("Revisa el email de verificación que te enviamos");
+//           window.location.href = "";
+//       })
+// }
+
+//     .then(result => {
+//         result.user.updateProfile({
+//             displayName : names
+//         })
+
+//         const configuration = {
+//             url : 'http://localhost:5000/'
+//         }
+
+//         result.user.sendVerificationEmail(configuration)
+//         .catch(error => {
+//             console.error(error)
+//             Materialize.toast(error.message, 4000)            
+//     })
+
+//     firebase.auth().signOut()
+
+//     Materialize.toast("Welcome ${names}, you must verify your account")
+
+//     $('.modal').modal('close')
+
+//     .catch(error => {
+//         console.error(error)
+//         Materialize.toast(error.message, 4000)
+//     })
+
+// }
